64ec9a8da004e41a9276e49954b8fce0e2356d60
Igor Shpakov <igorshp@gmail.com>
2014-07-18 16:33:31 +0100

    added a generic json version

--- lib/lita/handlers/makeitrain.rb
+++ lib/lita/handlers/makeitrain.rb
@@ -1,6 +1,7 @@
 require "lita"
 require "net/http"
 require "uri"
+require "json"
 
 module Lita
   module Handlers
@@ -9,9 +10,20 @@ module Lita
 
         def makeitrain(response)
             response.reply "http://f.cl.ly/items/2k1d1H270r0S1y0V1J2q/Will-MIR.gif"
+        end
+    end
+    class JsonGifs < Handler
+        route(/^(.*)/, :jsongifs, command: true, help: { "jsongifs" => "displays various gifs gifs!"})
 
+        def jsongifs(response)
+            res = Net::HTTP.get_response(URI('http://docs.crdsrg.net/jsongifs.json'))
+            gifs = JSON.parse(res.body)
+            if gifs.has_key?(response.matches[0][0])
+                response.reply gifs[response.matches[0][0]]
+            end
         end
     end
     Lita.register_handler(Makeitrain)
+    Lita.register_handler(JsonGifs)
   end
 end
--- spec/lita/handlers/makeitrain_spec.rb
+++ spec/lita/handlers/makeitrain_spec.rb
@@ -10,4 +10,16 @@ describe Lita::Handlers::Makeitrain, lita_handler: true do
       expect(replies.last).to match(URI.regexp)
     end
   end
+
+  
+end
+describe Lita::Handlers::JsonGifs, lita_handler: true do
+  it { routes_command("test").to(:jsongifs) }
+
+  describe "jsongifs" do
+    it "should return a url" do
+      send_command("test")
+      expect(replies.last).to match('found')
+    end
+  end
 end
