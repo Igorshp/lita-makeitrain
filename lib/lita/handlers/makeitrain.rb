19ca7c5ee88b82bc0605413ed2ffbd66cb589599
Igor Shpakov <igorshp@gmail.com>
2014-07-18 11:44:37 +0100

    update

--- lib/lita/handlers/makeitrain.rb
+++ lib/lita/handlers/makeitrain.rb
@@ -8,7 +8,7 @@ module Lita
         route(/^makeitrain/, :makeitrain, command: true, help: { "makeitrain" => "displays gifs!"})
 
         def makeitrain(response)
-            res = Net::HTTP.get_response(URI('http://cl.ly/image/2D180p0N2919'))
+            res = Net::HTTP.get_response(URI('http://f.cl.ly/items/2k1d1H270r0S1y0V1J2q/Will-MIR.gif'))
             response.reply(res['location'])
 
         end
